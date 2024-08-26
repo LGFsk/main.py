0ba3238a7e299363e158962007c68b0b28af9f8c
LGFsk <LGFs@mail.ru>
2024-08-26 20:57:11 +0300

    Домашняя работа по уроку "Функции в Python.Функция с параметром, доработано с учетом требований ДЗ

--- module_2_5.py
+++ module_2_5.py
@@ -1,13 +1,13 @@
 def matrix():
-    matrix = []
     n = int(input("Введите количество списков: ", ))
     m = int(input("Введите количество элементов списков: ", ))
     value = int(input("Введите значение: ", ))
-    for i in range(m):
-        matrix.append(value)
-    a = matrix[:]
-    matrix.clear()
+    matrix = []
     for i in range(n):
-        matrix.append(a)
+        matrix.append([] * m)
+        for j in range(m):
+            matrix[i].append(value)
     print(matrix)
+
+
 matrix()
