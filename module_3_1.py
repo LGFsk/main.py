1f902f50bc56241da3aadbb981f019726fcb0374
LGFsk <LGFs@mail.ru>
2024-08-30 16:58:54 +0300

    Домашняя работа по уроку "Пространство имён"

--- module_3_1.py
+++ module_3_1.py
@@ -1,12 +1,15 @@
-calls  = 0
+calls = 0
+
 
 def count_calls():
     global calls
     calls = calls + 1
 
+
 def string_info(string):
     count_calls()
-    return (len(string),string.upper(),string.lower())
+    return (len(string), string.upper(), string.lower())
+
 
 def is_contains(string, list_to_serach):
     count_calls()
@@ -16,8 +19,9 @@ def is_contains(string, list_to_serach):
             break
     return False
 
+
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
-print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(is_contains('URban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
 print(calls)
