6f55903340a57712af277da3026a7f0d0105accc
LGFsk <LGFs@mail.ru>
2024-09-02 11:33:36 +0300

    Домашняя работа по уроку "Способы вызова функции"

--- module_3_2.py
+++ module_3_2.py
@@ -1,1 +1,18 @@
-def print_params(a, b, c)
+def send_email(message, recipient, sender="university.help@gmail.com"):
+    if (".com" in sender or ".ru" in sender or ".net" in sender) and (
+            ".com" in recipient or ".ru" in recipient or ".net" in recipient) and ("@" in sender) and (
+            "@" in recipient):
+        if sender == "university.help@gmail.com":
+            print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}]")
+        elif sender == recipient:
+            print(f"Нельзя отправить письмо самому себе!")
+        else:
+            print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
+    else:
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
