95a2af6f2676cf04c50b91e823edc6b97e1055d1
LGFsk <LGFs@mail.ru>
2024-09-19 08:34:31 +0300

    Исправлено: Если число будет оканчиваться нулем, возвращаемое произведение будет равно 0.

--- module_3_5.py
+++ module_3_5.py
@@ -1,9 +1,11 @@
 def get_multiplied_digits(number):
     str_number = str(number)
+    s1_number = str(int(str_number[::-1]))
+    str_number= str(int(s1_number[::-1]))
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
-result = get_multiplied_digits(40203)
+result = get_multiplied_digits(24078000)
 print(result)
