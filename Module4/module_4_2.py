1382de7b45c672ce1d2990e1baf92bbbaabbb813
LGFsk <LGFs@mail.ru>
2024-10-02 14:21:25 +0300

    ДЗ 4.2

--- Module4/module_4_2.py
+++ Module4/module_4_2.py
@@ -1,11 +1,12 @@
 def test_function():
     def inner_function():
         print(f'Я в области видимости функции test_function()')
+
     inner_function()
 
-#inner_function()
+
 test_function()
-
-
-
-
+"""
+inner_function()
+Выдает ошибку так как вне области видомсти
+"""
