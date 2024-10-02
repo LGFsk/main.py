fb2610b1940a135869b3e6f532e24f33a12166c8
LGFsk <LGFs@mail.ru>
2024-10-02 14:22:14 +0300

    ДЗ 2.2

--- Module2/module_2_2.py
+++ Module2/module_2_2.py
@@ -2,15 +2,15 @@ first = int(input("Введите 1 число: "))
 second = int(input("Введите 2 число: "))
 third = int(input("Введите 3 число: "))
 
-#Первый вариант
+# Первый вариант
 if first == second and second == third:
     print(3)
 elif first != second and second != third and first != third:
-        print(0)
-elif first == second or second == third or first==third:
-            print("2")
+    print(0)
+elif first == second or second == third or first == third:
+    print("2")
 
-#второй вариант
+# второй вариант
 if first == second and second == third:
     print(3)
 else:
